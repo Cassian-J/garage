1cd6b0f0883a9848046180dc9c3574e41d4d6a33
cassian joly <cassian.joly@ynov.com>
2024-12-12 13:50:30 +0100

    class car create

--- car.cs
+++ car.cs
@@ -5,38 +5,28 @@ namespace CSharpDiscovery.Quest04
     public class Car 
     {
         public string Brand { get; set; }
-        public string Color { get; set; }
-        public int CurrentSpeed { get; set; }
+        public string Model { get; set; }
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+        public bool Statue { get; set; }
         public Car()
             {
+                Statue = false;
+                Year = 0;
+                Id = 0;
                 Brand = "Unknown";
-                Color = "Unknown";
-                CurrentSpeed = 0;
+                Model = "Unknown";
+                
             }
-        public Car(string model,string brand, string color, int currentSpeed = 0)
-            : base(brand, color,currentSpeed)
+        public Car(string model,string brand, int year, bool statue, int id)
             {
+                Statue = statue;
+                Year = year;
+                Id = id;
+                Brand = brand;
                 Model = model;
             }
-        public override string ToString()
-        {
-            return $"{Color} {Brand} {Model}";
-        }
-        public override void Accelerate(int speedIncrease)
-        {
-            CurrentSpeed += speedIncrease;
-            if (CurrentSpeed > 180)
-            {
-                CurrentSpeed = 180;
-            }
-        }
-        public override void Brake(int speedDecrease)
-        {
-            CurrentSpeed -= speedDecrease;
-            if (CurrentSpeed < 0)
-            {
-                CurrentSpeed = 0;
-            }
+        
         }
     }
-}
